ed7f73c838e090dc727b0e8ef4c3255cf9b3c3a2
miyunecadz <jvcadz@gmail.com>
2022-11-27 08:09:34 +0800

    unfinished design in register

--- src/router/index.js
+++ src/router/index.js
@@ -2,7 +2,6 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import LoginView from '../views/LoginView.vue'
 import RegisterView from '../views/RegisterView.vue'
-import AboutView from '../views/AboutView.vue'
 import SetView from '../views/SetView.vue'
 import DashboardView from '../views/DashboardView.vue'
 import ProfileView from '../views/ProfileView.vue'
@@ -25,11 +24,6 @@ const router = createRouter({
       path: '/register',
       name: 'register',
       component: RegisterView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: AboutView
     },
     {
       path: '/set',
--- src/views/RegisterView.vue
+++ src/views/RegisterView.vue
@@ -1,68 +1,69 @@
 <script setup>
-import NavBar from '../components/NavBar.vue'
 </script>
 <template>
-    <NavBar />
     <div class="container">
-        <div class="row justify-content-center" style="height:80vh">
-            <div class="col-md-8 my-auto">
-                <h4>Register Account</h4>
-                <form action="" method="post">
-                    <div class="row">
-                        <div class="col-md">
-
-                            <div class="form-group">
-                                <label for="id number">ID Number</label>
-                                <input type="text" name="id number" id="id number" class="form-control">
-                            </div>
-                            <div class="row gap-1">
-                                <div class="form-group col-md">
-                                    <label for="first">First Name</label>
-                                    <input type="text" name="first name" id="first name" class="form-control">
-                                </div>
-                                <div class="form-group col-md-4">
-                                    <label for="middle name">Middle Name</label>
-                                    <input type="text" name="middle name" id="middle name" class="form-control">
-                                </div>
-                            </div>
-                            <div class="form-group">
-                                <label for="last name">Last Name</label>
-                                <input type="text" name="last name" id="last name" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="gender">Gender</label>
-                                <select name="gender" id="gender" class="form-select">
-                                    <option value="">Select one</option>
-                                    <option value="male">Male</option>
-                                    <option value="female">Female</option>
-                                </select>
-                            </div>
-                            <div class="form-group">
-                                <label for="address">Address</label>
-                                <textarea name="address" id="address" class="form-control" cols="10"
-                                    rows="5"></textarea>
-                            </div>
-                        </div>
-                        <div class="col-md">
-                            <div class="form-group">
-                                <label for="contact number">Contact Number</label>
-                                <input type="text" name="contact number" id="contact number" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="email address">Email Address</label>
-                                <input type="text" name="email address" id="email address" class="form-control">
-                            </div>
-                            <div class="form-group">
-                                <label for="password">Password</label>
-                                <input type="password" name="password" id="password" class="form-control">
-                            </div>
-                        </div>
-                        
+        <div class="row justify-content-center p-5">
+            <div class="col-md-8 border p-2">
+                <form action="">
+                    <div class="form-group">
+                        <label for="id_number">ID Number or Employee ID Number</label>
+                        <input type="text" name="id_number" id="id_number" class="form-control">
                     </div>
-                    <div class="row justify-content-center">
-                        <div class="col me-auto">
-                            <button type="submit" class="btn btn-primary mt-2">Sign up</button>
-                        </div>
+                    <div class="form-group">
+                        <label for="first_name">First Name</label>
+                        <input type="text" name="first_name" id="first_name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="middle_name">Middle Name</label>
+                        <input type="text" name="middle_name" id="middle_name" class="form-control">
+                    </div>
+                    <div class="form-group">
+                        <label for="last_name">Last Name</label>
+                        <input type="text" name="last_name" id="last_name" class="form-control">
+                    </div>
+
+                    <!-- Gawskie palihug kog change -->
+                    <div class="form-group">
+                        <label for="status">Status</label>
+                        <select name="status" id="status" class="form-select">
+                            <option value="">Select one</option>
+                            <option value="student">Student</option>
+                            <option value="employee">Employee</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="gender">Gender</label>
+                        <select name="gender" id="gender" class="form-select">
+                            <option value="">Select one</option>
+                            <option value="male">Male</option>
+                            <option value="female">Female</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="address">Address</label>
+                        <textarea name="address" id="address" cols="30" rows="2" class="form-control"></textarea>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="contact-number">Contact Number</label>
+                        <input type="text" name="contact-number" id="contact-number" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Email Address</label>
+                        <input type="email" name="email" id="email" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password">Password</label>
+                        <input type="text" name="password" id="password" class="form-control">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="password_confirmation">Confirm Password</label>
+                        <input type="text" name="password_confirmation" id="password_confirmation" class="form-control">
                     </div>
                 </form>
             </div>
