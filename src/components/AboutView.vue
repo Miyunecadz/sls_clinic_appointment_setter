c501116edf3204c5d1d45661a5be309efc2df93a
miyunecadz <jvcadz@gmail.com>
2022-11-27 09:33:43 +0800

    Add card for specialist

--- src/components/AboutView.vue
+++ src/components/AboutView.vue
@@ -5,14 +5,21 @@
                 <div class="col-md-6 my-auto">
                     <h5 class="text-center"> Meet Our Specialist</h5>
                     <div class="d-flex justify-content-center gap-2">
-                        <div class="card my-3 mx-3" style="width: 150px">
+                        <div class="card my-3" style="width:150px;">
                             <img src="../assets/nurse.jpg"  class="card-img-top" alt="...">
                             <div class="card-body text-black text-center">
                                 <h6>Maria Emelee Bascug,RN MAN</h6>
                                 <span>Nurse II</span>
                             </div>
                         </div>
-                        <div class="card my-3 mx-3" style="width: 150px">
+                        <div class="card my-3" style="width:150px;">
+                            <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
+                            <div class="card-body text-black text-center">
+                                <h6>Dr. Edmundo Vill, Physician</h6>
+                                <span>University Doctor</span>
+                            </div>
+                        </div>
+                        <div class="card my-3" style="width:150px;">
                             <img src="../assets/doctor.jpg" class="card-img-top" alt="...">
                             <div class="card-body text-black text-center">
                                 <h6>Dr. Edmundo Vill, Physician</h6>
