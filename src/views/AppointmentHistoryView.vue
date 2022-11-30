f5e30fd424f2048e80d599de74aacf876f29d634
miyunecadz <jvcadz@gmail.com>
2022-11-30 11:31:01 +0800

    Fix Appointment History title

--- src/views/AppointmentHistoryView.vue
+++ src/views/AppointmentHistoryView.vue
@@ -6,48 +6,48 @@ import SidebarComponent from '../components/SidebarComponent.vue'
 
 
 <template>
-    <SidebarComponent>
-
-   
+  <SidebarComponent>
     <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-6 my-auto">
- 
-    <table class="table ">
-        <h3>
-    Appointment History
-</h3>
-  <thead>
-    <tr>
-      <th scope="col">Appointment ID</th>
-      <th scope="col">Date</th>
-      <th scope="col">Time</th>
-      <th scope="col">Status</th>
-    </tr>
-  </thead>
-  <tbody class="table-group-divider">
-    <tr>
-      <th scope="row">1</th>
-      <td>a</td>
-      <td>b</td>
-      <td>c</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>d</td>
-      <td>e</td>
-      <td>f</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td>g</td>
-      <td>h</td>
-      <td>i</td>
-    </tr>
-  </tbody>
-</table>
-</div>
-</div>
-</div>
-</SidebarComponent>
+      <div class="row justify-content-center">
+        <div class="col-md-6 my-auto">
+
+          <h3>Appointment History</h3>
+          <table class="table ">
+            <thead>
+              <tr>
+                <th scope="col">Appointment ID</th>
+                <th scope="col">Date</th>
+                <th scope="col">Time</th>
+                <th scope="col">Status</th>
+                <th scope="col">Action</th>
+              </tr>
+            </thead>
+            <tbody class="table-group-divider">
+              <tr>
+                <th scope="row">1</th>
+                <td>a</td>
+                <td>b</td>
+                <td>c</td>
+                <td>c</td>
+              </tr>
+              <tr>
+                <th scope="row">2</th>
+                <td>d</td>
+                <td>e</td>
+                <td>f</td>
+                <td>c</td>
+              </tr>
+              <tr>
+                <th scope="row">3</th>
+                <td>g</td>
+                <td>h</td>
+                <td>i</td>
+                <td>c</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </SidebarComponent>
 </template>
